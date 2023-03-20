460707ae9e3f7b87ba7ff97961621639d68c1ad1
Paul <paulnguyen0417@gmail.com>
2023-03-19 21:36:15 -0400

    Landing Page Top Bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import landingPageStyles from '../styles/landingpage.module.css';
+
 export default function Home() {
   return (
     <>
-      <div></div>
+      <div className={landingPageStyles.topbar}>
+        <div className={landingPageStyles.topbarLogo}>
+          <h1><a href="#">ExTracker</a></h1>
+        </div>
+        <ul className={landingPageStyles.topbarLinks}>
+          <li><a href="#overview">Overview</a></li>
+          <li><a href="#features">Features</a></li>
+          <li><a href="#team">Team</a></li>
+        </ul>
+        <div className={landingPageStyles.userReg}>
+          <button className={landingPageStyles.loginButton}>Log In</button>
+          <button className={landingPageStyles.rgstrButton}>Sign Up</button>
+        </div>
+      </div>
     </>
   );
 }
