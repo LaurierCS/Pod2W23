4cae97a16850f91481a8c17056bf8b6a3f624fcd
Paul <paulnguyen0417@gmail.com>
2023-03-20 03:42:48 -0400

    Added Section One Background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,26 @@ import landingPageStyles from '../styles/landingpage.module.css';
 export default function LandingPage() {
   return (
     <>
-      <div className={landingPageStyles.topbar}>
-        <a href="#">
-          <div className={landingPageStyles.topbarBrand}>
-            <div className={landingPageStyles.topbarLogo}>
-              <span className={landingPageStyles.circle}></span>
-              <span className={landingPageStyles.circle}></span>
+      <div className={landingPageStyles.sectionOne}>
+        <div className={landingPageStyles.topbar}>
+          <a href="#">
+            <div className={landingPageStyles.topbarBrand}>
+              <div className={landingPageStyles.topbarLogo}>
+                <span className={landingPageStyles.circle}></span>
+                <span className={landingPageStyles.circle}></span>
+              </div>
+              <h1>ExTracker</h1>
             </div>
-            <h1>ExTracker</h1>
+          </a>
+          <ul className={landingPageStyles.topbarLinks}>
+            <li><a href="#overview">Overview</a></li>
+            <li><a href="#features">Features</a></li>
+            <li><a href="#team">Team</a></li>
+          </ul>
+          <div className={landingPageStyles.userReg}>
+            <button className={landingPageStyles.loginButton}>Log In</button>
+            <button className={landingPageStyles.rgstrButton}>Sign Up</button>
           </div>
-        </a>
-        <ul className={landingPageStyles.topbarLinks}>
-          <li><a href="#overview">Overview</a></li>
-          <li><a href="#features">Features</a></li>
-          <li><a href="#team">Team</a></li>
-        </ul>
-        <div className={landingPageStyles.userReg}>
-          <button className={landingPageStyles.loginButton}>Log In</button>
-          <button className={landingPageStyles.rgstrButton}>Sign Up</button>
         </div>
       </div>
     </>
