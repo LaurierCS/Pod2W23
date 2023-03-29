c04bd18478e1e1ab0456b437bcd7ae536ee06783
Paul <paulnguyen0417@gmail.com>
2023-03-29 10:13:35 -0400

    Class Names + Styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,41 +3,39 @@ import landingPageStyles from '../styles/landingpage.module.css';
 export default function LandingPage() {
   return (
     <>
-      <div className={landingPageStyles.sectionOne}>
-        <div className={landingPageStyles.topbar}>
-          <a href="#">
-            <div className={landingPageStyles.topbarBrand}>
-              <div className={landingPageStyles.topbarLogo}>
-                <span className={landingPageStyles.circle}></span>
-                <span className={landingPageStyles.circle}></span>
+      <div className={landingPageStyles.mainSection}>
+          <div className={landingPageStyles.topbar}>
+            <a href="#">
+              <div className={landingPageStyles.topbarBrand}>
+                <div className={landingPageStyles.topbarLogo}>
+                  <span className={landingPageStyles.circle}></span>
+                  <span className={landingPageStyles.circle}></span>
+                </div>
+                <h1>ExTracker</h1>
               </div>
-              <h1>ExTracker</h1>
+            </a>
+            <ul className={landingPageStyles.topbarLinks}>
+              <li><a href="#overview">Overview</a></li>
+              <li><a href="#features">Features</a></li>
+              <li><a href="#team">Team</a></li>
+            </ul>
+            <div className={landingPageStyles.userReg}>
+              <button className={landingPageStyles.loginButton}>Log In</button>
+              <button className={landingPageStyles.rgstrButton}>Sign Up</button>
             </div>
-          </a>
-          <ul className={landingPageStyles.topbarLinks}>
-            <li><a href="#overview">Overview</a></li>
-            <li><a href="#features">Features</a></li>
-            <li><a href="#team">Team</a></li>
-          </ul>
-          <div className={landingPageStyles.userReg}>
-            <button className={landingPageStyles.loginButton}>Log In</button>
-            <button className={landingPageStyles.rgstrButton}>Sign Up</button>
           </div>
-        </div>
-        <div className={landingPageStyles.heroContent}>
-          <h1>An easy way to manage your expenses</h1>
-          <h6>Keep track of your spending across all bank accounts with ExTracker</h6>
-          <button className={landingPageStyles.getStartedButton}>Get Started</button>
-        </div>
-
-        <div className={landingPageStyles.ImageContainer}>
-          <div className={landingPageStyles.Image}>
-      
+          <div className={landingPageStyles.mainHero}>
+            <h1>An easy way to manage your expenses</h1>
+            <h6>Keep track of your spending across all bank accounts with ExTracker</h6>
+            <button className={landingPageStyles.getStartedButton}>Get Started</button>
           </div>
+        <div className={landingPageStyles.mainImageContainer}>
+          <div className={landingPageStyles.mainImage}></div>
         </div>
       </div>
 
-      <div className={landingPageStyles.sectionTwo}>
+      <div className={landingPageStyles.overviewSection}>
+
       </div>
     </>
   );
