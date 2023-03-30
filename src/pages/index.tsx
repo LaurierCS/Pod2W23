0745aa8ea0cc2ee8de6341e0da3c6b69ac0fff58
Paul <paulnguyen0417@gmail.com>
2023-03-30 02:18:35 -0400

    Overview Section + Styling Changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import landingPageStyles from '../styles/landingpage.module.css';
+import Image from 'next/image';
+import creditCard from '../images/creditcard.png';
 
 export default function LandingPage() {
   return (
@@ -14,11 +16,11 @@ export default function LandingPage() {
               <h1>ExTracker</h1>
             </div>
           </a>
-          <ul className={landingPageStyles.topbarLinks}>
+          {/* <ul className={landingPageStyles.topbarLinks}>
             <li><a href="#overview">Overview</a></li>
             <li><a href="#features">Features</a></li>
             <li><a href="#team">Team</a></li>
-          </ul>
+          </ul> */}
           <div className={landingPageStyles.userReg}>
             <button className={landingPageStyles.loginButton}>Log In</button>
             <button className={landingPageStyles.rgstrButton}>Sign Up</button>
@@ -27,17 +29,29 @@ export default function LandingPage() {
         <div className={landingPageStyles.mainHero}>
           <h1>An easy way to manage your expenses</h1>
           <h6>Keep track of your spending across all bank accounts with ExTracker</h6>
-          <button className={landingPageStyles.getStartedButton}>Get Started</button>
+          <button className={landingPageStyles.mainButton}>Get Started</button>
         </div>
         <div className={landingPageStyles.mainImageContainer}>
           <div className={landingPageStyles.mainImage}></div>
-          <div className={landingPageStyles.imageBackgroundExtension}></div>
+          <div className={landingPageStyles.mainBackgroundExtension}></div>
         </div>
       </div>
       
 
       <div className={landingPageStyles.overviewSection}>
-        <div><h1>TESTTTTT</h1></div>
+        <div className={landingPageStyles.overviewHero}>
+          <h1>Keep tabs on your expenses</h1>
+          <h6>We help you easily connect all your cash, credit, loan and investment accounts.  You can see your expenses and complete financial picture in ExTracker</h6>
+          <button className={landingPageStyles.overviewButton}>Get Started</button>
+        </div>
+        <div className={landingPageStyles.overviewImageContainer}>
+          <Image
+            src={creditCard}
+            alt="Credit Card"
+            className={landingPageStyles.overviewCardImage}
+          ></Image>
+          <div className={landingPageStyles.overviewImage}></div>
+        </div>
       </div>
     </>
   );
