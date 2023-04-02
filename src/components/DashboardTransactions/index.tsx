24fcab661df88407ba4c2e48b3849d23496de792
juancwu <46619361+juancwu@users.noreply.github.com>
2023-04-02 18:52:32 -0400

    remove link tags for google fonts:

--- src/components/DashboardTransactions/index.tsx
+++ src/components/DashboardTransactions/index.tsx
@@ -8,13 +8,6 @@ import Transactions from "./Transactions/Transactions";
 export default function DashboardTransactions() {
   return (
     <Container id={styles.transactionsTab}>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap"
-        rel="stylesheet"
-      />
-
       <Header></Header>
 
       <Columns></Columns>
