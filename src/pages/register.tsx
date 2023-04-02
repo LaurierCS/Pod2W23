b6d25377e350c0c09c812e8c76432542d9c8b057
Paul <paulnguyen0417@gmail.com>
2023-04-01 22:52:09 -0400

    Form UI + Validation w/ Yup Library

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,30 @@
+// Mantine Library
+import { useForm, yupResolver } from '@mantine/form';
+import { TextInput } from '@mantine/core';
+// Yup Validation Library
+import * as Yup from 'yup';
 // Styles | Images
 import styles from '../styles/auth.module.css';
 
 function Register() {
+    let userSchema = Yup.object({
+        firstName: Yup.string().required('First name is a required field'),
+        lastName: Yup.string().required('Last name is a required field'),
+        email: Yup.string().required('Email is a required field').email('Email must be valid'),
+        password: Yup.string().min(8, 'Password must be between 8-16 characters').max(16, 'Password must be between 8-16 characters'),
+    });
+
+    const form = useForm({
+        initialValues: {
+          firstName: '',
+          lastName: '',
+          email: '',
+          password: ''
+        },
+    
+        validate: yupResolver(userSchema),
+    })
+
     return  (
         <>
             <div className={styles.authBackground}>
@@ -22,6 +45,58 @@ function Register() {
                         <h1>Create New Account</h1>
                         <h3>Already A Member? <span>Log in</span></h3>
                     </div>
+                    <form className={styles.userForm} onSubmit={form.onSubmit(() => {})}>
+                        <div className={styles.nameForm}>
+                            <TextInput 
+                                classNames={{ 
+                                    label: styles.inputLabel,
+                                    input: styles.inputField,
+                                    error: styles.inputError,
+                                }}
+                                label="First Name" 
+                                placeholder="First Name" 
+                                size="lg"
+                                withAsterisk
+                                {...form.getInputProps('firstName')} />
+                            <TextInput 
+                                classNames={{ 
+                                    label: styles.inputLabel,
+                                    input: styles.inputField,
+                                    error: styles.inputError,
+                                }}
+                                label="Last Name" 
+                                placeholder="Last Name" 
+                                size="lg"
+                                withAsterisk
+                                {...form.getInputProps('lastName')} />
+                        </div>
+                        <TextInput 
+                            classNames={{ 
+                                label: styles.inputLabel,
+                                input: styles.inputField,
+                                error: styles.inputError,
+                            }}
+                            label="Email" 
+                            placeholder="Email" 
+                            size="lg"
+                            withAsterisk
+                            {...form.getInputProps('email')} />
+                        <TextInput 
+                            classNames={{ 
+                                label: styles.inputLabel,
+                                input: styles.inputField,
+                                error: styles.inputError,
+                            }}
+                            label="Password" 
+                            placeholder="Password" 
+                            size="lg"
+                            withAsterisk
+                            {...form.getInputProps('password')} />
+                        
+                        <div className={styles.regButtonContainer}>
+                        <button className={styles.regButton} type="submit">Create Account</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
