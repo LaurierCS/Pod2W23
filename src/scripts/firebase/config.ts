2ce6a84c2859be5659bdf09814a59d3cd275e560
Chisholm6192 <rmwc92@gmail.com>
2023-05-25 10:55:41 -0400

    Chnages to firebase package that was installed

--- src/scripts/firebase/config.ts
+++ src/scripts/firebase/config.ts
@@ -7,15 +7,15 @@ import { getFirestore } from "firebase/firestore";
 
 // paste config
 const firebaseConfig = {
-    apiKey: "AIzaSyC0uX-MWgWGDouhJJWLCejBNT3IfaNKVZI",
-    authDomain: "expense-tracker-5b047.firebaseapp.com",
-    databaseURL: "https://expense-tracker-5b047-default-rtdb.firebaseio.com",
-    projectId: "expense-tracker-5b047",
-    storageBucket: "expense-tracker-5b047.appspot.com",
-    messagingSenderId: "479377948780",
-    appId: "1:479377948780:web:55b9af35e2518ce3d6d849",
-    measurementId: "G-8HYESJ1JE5"
-  };
+  apiKey: "AIzaSyC0uX-MWgWGDouhJJWLCejBNT3IfaNKVZI",
+  authDomain: "expense-tracker-5b047.firebaseapp.com",
+  databaseURL: "https://expense-tracker-5b047-default-rtdb.firebaseio.com",
+  projectId: "expense-tracker-5b047",
+  storageBucket: "expense-tracker-5b047.appspot.com",
+  messagingSenderId: "479377948780",
+  appId: "1:479377948780:web:55b9af35e2518ce3d6d849",
+  measurementId: "G-8HYESJ1JE5"
+};
 
 console.log(firebaseConfig);
 
